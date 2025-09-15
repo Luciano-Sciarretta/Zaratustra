0c5247f33b9233683a73108a0278566ab9971269
Luciano Sciarretta <clsciarretta@hotmail.com>
2025-09-15 17:43:57 -0300

    Sqlite3

--- Zaratustra/settings.py
+++ Zaratustra/settings.py
@@ -11,24 +11,13 @@ SECRET_KEY = 'django-insecure-9+&o&a+vb5*ije8m)!cyxgdll2b4+6rnwc6ef6duu!y$c_$3b+
 
 DEBUG = True
 
-if DEBUG:
-    DATABASES = {
+DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB"),
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "HOST": os.environ.get("POSTGRES_HOST"),
-            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
-        }
-    }
+
 
 
 ALLOWED_HOSTS = ["zaratustra.onrender.com", "127.0.0.1", "localhost"]
@@ -137,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-TATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 
