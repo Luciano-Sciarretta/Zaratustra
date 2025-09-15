2376017754f97d06b0def19dc4f4bbc89e4cdecb
Luciano Sciarretta <clsciarretta@hotmail.com>
2025-09-15 10:38:35 -0300

    Antes de subir a render

--- Zaratustra/settings.py
+++ Zaratustra/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-9+&o&a+vb5*ije8m)!cyxgdll2b4+6rnwc6ef6duu!y$c_$3b+
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Zaratustra.onrender.com"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
